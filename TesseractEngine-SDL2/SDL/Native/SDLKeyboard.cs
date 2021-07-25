25c489a6690dfcf021c4caf8c7a70df281bcba72
Zekrom_64 <Zekrom64@users.noreply.github.com>
2021-07-25 07:59:34 -0500

    More SDL implementation + Added SDL cursor, joystick, and keyboard support + Reworked SDL pixel formats and palettes + Added SDL image processing service

---
+++ TesseractEngine-SDL2/SDL/Native/SDLKeyboard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesseract.SDL.Native {
+	
+	public struct SDL_Keysym {
+		public SDLScancode scancode;
+		public SDLKeycode sym;
+		public ushort mod;
+		public uint unused;
+	}
+
+}
---
+++ TesseractEngine-SDL2/SDL/SDLKeyboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Tesseract.SDL {
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SDLKeysym {
+
+		public SDLScancode Scancode;
+		public SDLKeycode Sym;
+		public SDLKeymod Mod;
+		private readonly uint unused;
+
+	}
+
+}
