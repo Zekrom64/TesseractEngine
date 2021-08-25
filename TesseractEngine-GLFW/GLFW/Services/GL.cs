d7cf070c62cd26b5038fa7ca9b2b1099db99a910
Zekrom_64 <Zekrom64@users.noreply.github.com>
2021-08-25 08:46:14 -0500

    Naming fixups + Vulkan windows

--- TesseractEngine-GLFW/GLFW/Services/GL.cs
+++ TesseractEngine-GLFW/GLFW/Services/GL.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tesseract.GL;
+using Tesseract.OpenGL;
 
 namespace Tesseract.GLFW.Services {
 
