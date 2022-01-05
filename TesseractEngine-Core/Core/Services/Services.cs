4a729592ef3cfd06b4a2e375a6beabf10013e393
Zekrom_64 <Zekrom64@users.noreply.github.com>
2022-01-04 19:22:38 -0600

    Added service injector

--- TesseractEngine-Core/Core/Services/Services.cs
+++ TesseractEngine-Core/Core/Services/Services.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,11 @@ namespace Tesseract.Core.Services {
 
 	public interface IServiceProvider {
 
-		public T GetService<T>(IService<T> service) => default;
+		public T GetService<T>(IService<T> service) {
+			Func<IServiceProvider,T> injectedGetter = ServiceInjector.Lookup<T>(this, service);
+			if (injectedGetter != null) return injectedGetter(this);
+			else return default;
+		}
 	
 	}
 
@@ -30,6 +35,24 @@ namespace Tesseract.Core.Services {
 		}
 
 		public static void AddGlobalService<T>(IService<T> service, T value) => services[service] = value;
+
+	}
+
+	public class ServiceInjector {
+
+		private static readonly Dictionary<(Type, Type), object> registry = new();
+
+		public static void Inject<P,S,T>(Func<P,T> getter) where P : IServiceProvider where S : IService<T> {
+			registry[(typeof(P),typeof(S))] = getter;
+		}
+
+		public static Func<IServiceProvider,T> Lookup<T>(IServiceProvider provider, IService<T> service) {
+			Type tProvider = provider.GetType(), tService = service.GetType();
+			if (registry.TryGetValue((tProvider, tService), out object fn)) {
+				MethodInfo mInvoke = fn.GetType().GetMethod("Invoke");
+				return (IServiceProvider p) => (T)mInvoke.Invoke(fn, new object[] { p });
+			} else return null;
+		}
 
 	}
 
