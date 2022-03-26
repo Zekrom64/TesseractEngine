7629064c7a2d75de6eb952f8281bd07ba8e484a3
Zekrom_64 <Zekrom64@users.noreply.github.com>
2022-03-26 12:07:20 -0500

    Initial commit

---
+++ TesseractEngine-ImGui/ImObjects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tesseract.ImGui {
+	
+	public class ImFontAtlas {
+
+	}
+
+	public class ImGuiIO {
+
+	}
+
+	public class ImGuiStyle {
+
+	}
+
+	public class ImDrawData {
+
+	}
+
+}
