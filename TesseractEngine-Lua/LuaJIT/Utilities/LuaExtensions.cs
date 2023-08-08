e2360b80be11d0ff919dadab05d2562941e09db3
Zekrom_64 <Zekrom64@users.noreply.github.com>
2023-08-08 08:46:14 -0500

    Added binary serialization extension

--- TesseractEngine-Lua/LuaJIT/Utilities/LuaExtensions.cs
+++ TesseractEngine-Lua/LuaJIT/Utilities/LuaExtensions.cs
@@ -262,7 +262,7 @@ namespace Tesseract.LuaJIT.Utilities {
 		/// 
 		/// </list>
 		/// </summary>
-		/// <param name="lua"></param>
+		/// <param name="lua">This Lua state</param>
 		public static void OpenJSON(this LuaState lua) {
 			Register(lua, "json"u8, "encode"u8, state => {
 				JsonWriterOptions opts = default;
@@ -292,6 +292,332 @@ namespace Tesseract.LuaJIT.Utilities {
 					JsonDecode(state, json.Value);
 					return 1;
 				} else throw new LuaException("Unexpected end of JSON");
+			});
+		}
+
+		/// <summary>
+		/// Enumeration of tokens for serialized data.
+		/// </summary>
+		public enum SerialToken : byte {
+			/// <summary>
+			/// End token, indicates the end of the data stream or the end of a table.
+			/// </summary>
+			End =       (byte)'e',
+			/// <summary>
+			/// Nil value token.
+			/// </summary>
+			Nil =       (byte)'n',
+			/// <summary>
+			/// True boolean value token.
+			/// </summary>
+			True =      (byte)'t',
+			/// <summary>
+			/// False boolean value token.
+			/// </summary>
+			False =     (byte)'f',
+			/// <summary>
+			/// Token for a number packed as a 7-bit integer.
+			/// </summary>
+			PackedInt = (byte)'i',
+			/// <summary>
+			/// Token for a number stored as a double.
+			/// </summary>
+			Double =    (byte)'d',
+			/// <summary>
+			/// String value token.
+			/// </summary>
+			String =    (byte)'s',
+			/// <summary>
+			/// Table value token.
+			/// </summary>
+			Table =     (byte)'T',
+			/// <summary>
+			/// Custom value token.
+			/// </summary>
+			Custom =    (byte)'C'
+		}
+
+		/// <summary>
+		/// The maximum size custom serialized data is allowed to be. This can be used to prevent a malicious
+		/// user from trying to deserialize a crafted value with a very large size.
+		/// </summary>
+		public static int SerializedDataMaxSize { get; set; } = int.MaxValue;
+
+		/// <summary>
+		/// Serializes the Lua value at the given stack index into binary data using a <see cref="BinaryWriter"/>.
+		/// </summary>
+		/// <param name="lua">The Lua interface</param>
+		/// <param name="bw">The writer to serialize data to</param>
+		/// <param name="index">The stack index of the value to serialize</param>
+		/// <exception cref="LuaException">If there was an error during serialization</exception>
+		public static void Serialize(this LuaState lua, BinaryWriter bw, int index) {
+			bool TrySerializeCustom(int index) {
+				bool isCustom = false;
+				if (lua.GetMetatable(index)) {
+					lua.GetField(-1, "__serialize"u8);
+					if (!(isCustom = lua.Type(-1) == LuaType.Function)) lua.Pop();
+				}
+
+				if (isCustom) {
+					// Call the __serialize metamethod with the object, returns a key and data string
+					lua.PushValue(index);
+					lua.Call(1, 2);
+					if (!(lua.IsString(-2) && lua.IsString(-1)))
+						throw new LuaException("Custom serialization function must return key and data as strings");
+					// Write data to the stream
+					bw.Write((byte)SerialToken.Custom);
+					ReadOnlySpan<byte> key = lua.ToStringBytes(-2);
+					if (key.Length > SerializedDataMaxSize) throw new LuaException("Custom value key too large");
+					bw.Write7BitEncodedInt(key.Length);
+					bw.Write(key);
+					ReadOnlySpan<byte> data = lua.ToStringBytes(-1);
+					if (data.Length > SerializedDataMaxSize) throw new LuaException("Custom value data too large");
+					bw.Write7BitEncodedInt(data.Length);
+					bw.Write(data);
+					return true;
+				} else return false;
+			}
+
+			switch (lua.Type(index)) {
+				case LuaType.Nil:
+					bw.Write((byte)SerialToken.Nil);
+					break;
+				case LuaType.Boolean:
+					if (lua.ToBoolean(index)) bw.Write((byte)SerialToken.True);
+					else bw.Write((byte)SerialToken.False);
+					break;
+				case LuaType.Number: {
+						double num = lua.ToNumber(index);
+						if (double.IsInteger(num) && num >= int.MinValue && num <= int.MaxValue) {
+							bw.Write((byte)SerialToken.PackedInt);
+							bw.Write7BitEncodedInt((int)num);
+						} else {
+							bw.Write((byte)SerialToken.Double);
+							bw.Write(num);
+						}
+					}
+					break;
+				case LuaType.String: {
+						ReadOnlySpan<byte> strBytes = lua.ToStringBytes(index);
+						if (strBytes.Length > SerializedDataMaxSize) throw new LuaException("String too large");
+						// <Token:B> <Length:7bI> <Data:nB>
+						bw.Write((byte)SerialToken.String);
+						bw.Write7BitEncodedInt(strBytes.Length);
+						bw.Write(strBytes);
+					}
+					break;
+				case LuaType.Table: {
+						index = lua.ToAbsoluteIndex(index);
+						int top = lua.Top;
+						try {
+							// Try to use the custom serialization function, else fall back to default
+							// method for serializing tables
+							if (!TrySerializeCustom(index)) {
+								// <Token:B> [<Key:?> <Value:?]... <End:B>
+								bw.Write((byte)SerialToken.Table);
+								lua.PushNil();
+								while (lua.Next(index)) {
+									Serialize(lua, bw, -2);
+									Serialize(lua, bw, -1);
+									lua.Pop();
+								}
+								bw.Write((byte)SerialToken.End);
+							}
+						} finally {
+							lua.Top = top;
+						}
+					}
+					break;
+				case LuaType.UserData: {
+						index = lua.ToAbsoluteIndex(index);
+						int top = lua.Top;
+						try {
+							if (!TrySerializeCustom(index))
+								throw new LuaException("Cannot serialize userdata without a custom serialization function");
+						} finally {
+							lua.Top = top;
+						}
+					} 
+					break;
+				case LuaType.None:
+				case LuaType.LightUserData:
+				case LuaType.Function:
+				case LuaType.Thread:
+				default:
+					throw new LuaException("Encountered unserializable value");
+			}
+			// Write a trailing end token just in case
+			bw.Write((byte)SerialToken.End);
+		}
+
+		/// <summary>
+		/// Deserializes a value from serialized binary data, pushing it onto the stack And
+		/// returning true. If the end of data is reached, nothing is pushed and false is returned.
+		/// </summary>
+		/// <param name="lua">The Lua interface</param>
+		/// <param name="br">The reader for serialized data</param>
+		/// <returns>If a value was deserialized and pushed</returns>
+		/// <exception cref="IOException">If there is an error reading the serialized data</exception>
+		/// <exception cref="LuaException">If there is an error decoding the serialized data</exception>
+		public static bool Deserialize(this LuaState lua, BinaryReader br) {
+			void ReadFully(Span<byte> span) {
+				int offset = 0;
+				do {
+					int nread = br.Read(span[offset..]);
+					if (nread == 0) throw new IOException("Unexpected end of serialized data");
+					offset += nread;
+				} while (offset < span.Length);
+			}
+
+			switch ((SerialToken)br.ReadByte()) {
+				case SerialToken.Nil:
+					lua.PushNil();
+					break;
+				case SerialToken.True:
+					lua.PushBoolean(true);
+					break;
+				case SerialToken.False:
+					lua.PushBoolean(false);
+					break;
+				case SerialToken.PackedInt:
+					lua.PushInteger(br.Read7BitEncodedInt());
+					break;
+				case SerialToken.Double:
+					lua.PushNumber(br.ReadDouble());
+					break;
+				case SerialToken.String: {
+						int length = br.Read7BitEncodedInt();
+						if (length > SerializedDataMaxSize) throw new LuaException("String has excessively large size");
+						Span<byte> buffer = length > 4096 ? new byte[length] : stackalloc byte[length];
+						ReadFully(buffer);
+						lua.PushString(buffer);
+					}
+					break;
+				case SerialToken.Table: {
+						int top = lua.Top;
+						try {
+							lua.CreateTable();
+							while (Deserialize(lua, br)) {
+								if (!Deserialize(lua, br)) throw new IOException("Table ends prematurely");
+								lua.RawSet(-3);
+							}
+							top++;
+						} finally {
+							lua.Top = top;
+						}
+					}
+					break;
+				case SerialToken.Custom: {
+						int top = lua.Top;
+						try {
+							int keyLen = br.Read7BitEncodedInt();
+							if (keyLen > SerializedDataMaxSize) throw new LuaException("Custom data has excessively large key");
+							Span<byte> key = keyLen > 4096 ? new byte[keyLen] : stackalloc byte[keyLen];
+							ReadFully(key);
+
+							bool hasDeserial = false;
+
+							lua.GetField(Lua.RegistryIndex, "__deserial"u8);
+							if (lua.IsTable(-1)) {
+								lua.PushString(key);
+								lua.RawGet(-2);
+								hasDeserial = lua.Type(-1) == LuaType.Function;
+							}
+
+							if (!hasDeserial) throw new LuaException($"Missing deserializer for custom key \"{Encoding.UTF8.GetString(key)}\"");
+
+							int dataLen = br.Read7BitEncodedInt();
+							if (dataLen > SerializedDataMaxSize) throw new LuaException("Custom data is excessively large");
+							Span<byte> data = dataLen > 4096 ? new byte[dataLen] : stackalloc byte[dataLen];
+							ReadFully(data);
+
+							lua.Remove(-2);
+							lua.PushString(data);
+							lua.Call(1, 1);
+						} finally {
+							lua.Top = top;
+						}
+					}
+					break;
+				case SerialToken.End:
+				default:
+					return false;
+			}
+			return true;
+		}
+
+		/// <summary>
+		/// Adds a custom deserialization handler like with <c>serialize.newdecoder()</c> (see
+		/// <see cref="OpenSerialize(LuaState)"/>).
+		/// </summary>
+		/// <param name="lua">The Lua interface</param>
+		/// <param name="func">The serialization function</param>
+		public static void AddCustomDeserializer(LuaState lua, string key, LuaFunction func) {
+			lua.GetField(Lua.RegistryIndex, "__deserial"u8);
+			if (lua.IsNil(-1)) {
+				lua.Pop();
+				lua.CreateTable();
+				lua.Dup();
+				lua.SetField(Lua.RegistryIndex, "__deserial"u8);
+			}
+			lua.PushFunction(func);
+			lua.SetField(-2, key);
+			lua.Pop();
+		}
+
+		/// <summary>
+		/// Adds a serialization package for converting between Lua values and binary data:
+		/// <list type="table">
+		/// 
+		/// <item>
+		/// <term><c>serialize.encode(value: any) -> string</c></term>
+		/// <description>Encodes a Lua value as serialized binary data. Userdata and tables
+		/// may support custom serialization via the <c>__serialize</c> metamethod. If present,
+		/// this is called with the object being serialized and must return a unique key string
+		/// followed by the serialized data packed in a string.</description>
+		/// </item>
+		/// 
+		/// <item>
+		/// <term><c>serialize.decode(data: string) -> any</c></term>
+		/// <description>Decodes a string of JSON text into a corresponding Lua value.</description>
+		/// </item>
+		/// 
+		/// <item>
+		/// <term><c>serialize.newdecoder(key: string, func: function)</c></term>
+		/// <description>Adds a custom deserializer for custom data with the given key. The deserialization
+		/// function is called with a string storing the custom data and must return the deserialized
+		/// data type.</description>
+		/// </item>
+		/// 
+		/// </list>
+		/// </summary>
+		/// <param name="lua">This Lua state</param>
+		public static void OpenSerialize(this LuaState lua) {
+			Register(lua, "serialize"u8, "encode"u8, state => {
+				using MemoryStream ms = new();
+				Serialize(state, new BinaryWriter(ms), 1);
+				state.PushString(ms.GetBuffer());
+				return 1;
+			});
+			Register(lua, "serialize"u8, "decode"u8, state => {
+				state.CheckString(1);
+				using MemoryStream ms = new(lua.ToStringBytes(1).ToArray());
+				if (!Deserialize(lua, new BinaryReader(ms))) state.PushNil();
+				return 1;
+			});
+			Register(lua, "serialize"u8, "newdecoder"u8, state => {
+				state.CheckString(1);
+				if (state.Type(2) != LuaType.Function) state.TypeError(2, "function");
+				state.GetField(Lua.RegistryIndex, "__deserial"u8);
+				if (lua.IsNil(-1)) {
+					lua.CreateTable();
+					lua.Dup();
+					lua.SetField(Lua.RegistryIndex, "__deserial"u8);
+				}
+				lua.PushValue(1);
+				lua.PushValue(2);
+				lua.RawSet(-3);
+				return 0;
 			});
 		}
 
